4b8599d3849cb08a7482c13c87cba6a076023b03
Chetan Mehta <chetanmehta@Chetans-MacBook-Pro.local>
2025-06-07 17:38:18 +0530

    mend

--- config/puma.rb
+++ config/puma.rb
@@ -1,26 +1,26 @@
-# # config/puma.rb
+# config/puma.rb
 
-# directory '/home/ubuntu/sakhiagrotech/current'
-# rackup "/home/ubuntu/sakhiagrotech/current/config.ru"
-# environment 'production'
+directory '/home/ubuntu/sakhiagrotech/current'
+rackup "/home/ubuntu/sakhiagrotech/current/config.ru"
+environment 'production'
 
-# # Set the number of Puma workers and threads
-# workers 2
-# threads 4, 16
+# Set the number of Puma workers and threads
+workers 2
+threads 4, 16
 
-# # Set the bind address and location for sockets
-# bind "unix:///home/ubuntu/sakhiagrotech/shared/tmp/sockets/puma.sock"
+# Set the bind address and location for sockets
+bind "unix:///home/ubuntu/sakhiagrotech/shared/tmp/sockets/puma.sock"
 
-# # PID and state paths
-# pidfile "/home/ubuntu/sakhiagrotech/shared/tmp/pids/puma.pid"
-# state_path "/home/ubuntu/sakhiagrotech/shared/tmp/pids/puma.state"
+# PID and state paths
+pidfile "/home/ubuntu/sakhiagrotech/shared/tmp/pids/puma.pid"
+state_path "/home/ubuntu/sakhiagrotech/shared/tmp/pids/puma.state"
 
-# stdout_redirect "/home/ubuntu/sakhiagrotech/shared/log/puma.stdout.log", "/home/ubuntu/sakhiagrotech/shared/log/puma.stderr.log"
+stdout_redirect "/home/ubuntu/sakhiagrotech/shared/log/puma.stdout.log", "/home/ubuntu/sakhiagrotech/shared/log/puma.stderr.log"
 
-# # Preload the application for faster worker boot
-# preload_app!
+# Preload the application for faster worker boot
+preload_app!
 
-# # Allow Puma to be restarted by `rails restart` or `pumactl restart`
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection
-# end
+# Allow Puma to be restarted by `rails restart` or `pumactl restart`
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
